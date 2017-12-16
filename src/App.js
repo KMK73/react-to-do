8ac775320db79869b60255cb924f196bf71412f1
Kelsey Kjeldsen <kelsey.kjeldsen@gmail.com>
2017-12-16 00:10:50 -0500

    checkpoint-7

--- src/App.js
+++ src/App.js
@@ -3,12 +3,38 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  //Accept props as a parameter.
+  //Call super(props).
+  constructor(props){
+    super(props);
+
+    // set initial states , state must be object
+    // The state object can be accessed from anywhere in the class.
+    this.state = {
+      todos: [
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isCompleted: false }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
           <ul>
-            <ToDo />
-            <ToDo />
+            {
+              this.state.todos.map(
+                (todo, index) =>
+                  /*
+                  We also specify a key attribute on ToDo, and we assign it the index.
+                   For React to operate properly, each child of an array or iterator needs
+                  to have a key with a unique value.
+                  The key gives React a reliable way of distinguishing between array items.
+                  */
+                  <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted}/>
+
+              )}
           </ul>
       </div>
     );
