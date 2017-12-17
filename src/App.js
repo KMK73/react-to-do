6bd56d59f582014c3834b360b58a23bf8a71e04a
Kelsey Kjeldsen <kelsey.kjeldsen@gmail.com>
2017-12-17 15:09:04 -0500

    checkpoint 8

--- src/App.js
+++ src/App.js
@@ -15,8 +15,44 @@ class App extends Component {
         { description: 'Walk the cat', isCompleted: true },
         { description: 'Throw the dishes away', isCompleted: false },
         { description: 'Buy new dishes', isCompleted: false }
-      ]
+      ],
+      newTodoDescription: ''
     };
+  }
+
+  toggleComplete(index) {
+    console.log('toggle complete executed', index);
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    // reads if todo is completed true, expression one change to false. else true
+    todo.isCompleted = todo.isCompleted ? false : true;
+    // set the state to the const todos array
+    this.setState({
+      todos: todos
+    })
+  }
+
+  handleChange(e){
+    // console.log('handle change ', e.target.value);
+    this.setState({
+      newTodoDescription: e.target.value
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    //if empty
+    if(!this.state.newTodoDescription){
+      return;
+    }
+
+    const newTodo = { description: this.state.newTodoDescription, isCompleted: false};
+    // uses spread ... syntax to add new element
+    // set description to empty to clear the input
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+      newTodoDescription: ''
+    });
   }
 
   render() {
@@ -31,11 +67,18 @@ class App extends Component {
                    For React to operate properly, each child of an array or iterator needs
                   to have a key with a unique value.
                   The key gives React a reliable way of distinguishing between array items.
+                  pass toggleComplete as an anonymous function with an index value
                   */
-                  <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted}/>
+                  <ToDo key={index} description={todo.description}
+                  isCompleted={todo.isCompleted}
+                  toggleComplete={ () => this.toggleComplete(index) }/>
 
               )}
           </ul>
+          <form onSubmit= { (e) => this.handleSubmit(e) }>
+            <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e)} />
+            <input type="submit" />
+          </form>
       </div>
     );
   }
